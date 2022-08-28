8a54cecee8ef10cb923cf42ed7987ae3167ee14b
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-08-28 01:52:07 +0000

    Add files via upload

---
+++ 7kyu/shortestWord.js
@@ -0,0 +1,32 @@
+// Shortest word
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+
+
+
+
+
+
+function findShort(s){
+  
+    let words = s.split(' ')
+    let shortest = words[0].length
+    let next 
+    
+    for( let i = 0; i < words.length; i++) {
+      next = words[i].length
+    if(shortest > next) shortest = next
+  }
+  
+  return shortest 
+  
+  }
+
+
+
+//   function findShort(s){
+//     return Math.min(...s.split(" ").map (s => s.length));
+// }
