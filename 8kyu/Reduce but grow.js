0401a2f39f9d4d47a0051013d0aa6d4f83e04c0f
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-08-28 02:20:23 +0000

    Add files via upload

---
+++ 8kyu/Reduce but grow.js
@@ -0,0 +1,16 @@
+// Reduce but grow
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+ 
+ 
+ grow = x => x.reduce( (acc, c) => acc*c)
+
+
+//  const grow = x => {
+//     let res = 1;
+//     for (let i = 0; i < x.length; i++) {
+//       res *= x[i];
+//     }
+//     return res;
+//   };
