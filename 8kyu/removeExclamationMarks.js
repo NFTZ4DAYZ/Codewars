7318dabdb0554eddd82f055c52dd90af0bdbeea9
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-09-06 23:23:34 +0000

    Add files via upload

---
+++ 8kyu/removeExclamationMarks.js
@@ -0,0 +1,17 @@
+// Remove exclamation marks   
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    return s.split('!').join('');
+  }
+
+
+
+  function removeExclamationMarks(s) {
+    var arr =s.split("");
+    arr = arr.filter(function(e){
+          return e !== "!";
+      })
+      return arr.join("");
+  }
