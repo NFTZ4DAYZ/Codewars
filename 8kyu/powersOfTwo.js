df0c1e97490a3400b90ff2167cf8ad91a0ddd4d3
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-09-09 22:07:53 +0000

    Add files via upload

---
+++ 8kyu/powersOfTwo.js
@@ -0,0 +1,24 @@
+// Powers of Two  
+
+
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging
+
+// from 0 to n ( inclusive ).
+
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+
+function powersOfTwo(n){
+    var result = [];
+    for (var i = 0; i <= n; i++) {
+      result.push(Math.pow(2, i));
+    }
+    return result;
+  }
+
+//   function powersOfTwo(n) {
+//     return [...Array(n + 1)].map((_, i) => 2 ** i)
+//   }
