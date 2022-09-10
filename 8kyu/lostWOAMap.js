92635ac2916b28c122592dbada8209eafe91a39e
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-09-10 23:17:37 +0000

    Add files via upload

---
+++ 8kyu/lostWOAMap.js
@@ -0,0 +1,21 @@
+// Lost without a map  
+
+
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+function maps(x){
+let arr = [];
+for(let i = 0; i < x.length; i++){
+arr.push(x[i] * 2);
+}
+return arr;
+}
+
+
+// function maps(x){
+//     return x.map(n => n * 2);
+//   }
