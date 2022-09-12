45456cace37ffc618094090734c476865fc07dd0
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-09-12 06:12:52 +0000

    Add files via upload

---
+++ 8kyu/5WithoutNum.js
@@ -0,0 +1,18 @@
+// 5 w/o num 
+
+
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+
+
+function unusualFive() {
+    return 'fucku'.length
+  }
+
+// const unusualFive = () =>
+//   Math.hypot(Math.ceil(Math.PI), Math.floor(Math.PI));
