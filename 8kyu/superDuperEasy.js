b961c57064b89359b0054e142b26ff58877eed6b
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-09-12 07:59:23 +0000

    Add files via upload

---
+++ 8kyu/superDuperEasy.js
@@ -0,0 +1,16 @@
+// Super Duper Easy
+
+
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
+
+
+// function problem(x){
+//     let sum = x * 50 + 6;
+//     if(x > 0 || x < 0 || x === 0)
+//     return sum
+//     else {
+//     return "Error"
+//   }
+//   }
