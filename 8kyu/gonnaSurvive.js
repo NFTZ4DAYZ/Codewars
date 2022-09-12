9e91264d3a8b137930be0b680b68d6393ef5aabc
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-09-12 23:40:42 +0000

    Add files via upload

---
+++ 8kyu/gonnaSurvive.js
@@ -0,0 +1,11 @@
+// Is he gonna Survive  
+
+// Two bullets to kill dragon.  Will the hero survivie????
+
+
+function hero(bullets, dragons){
+    return bullets >= dragons * 2
+  }
+
+
+//   const hero = (bullets, dragons) => dragons * 2 <= bullets;
