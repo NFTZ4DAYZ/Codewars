e65d5bf34be7d52607b196a27a71b80b85bec5bd
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-09-17 05:35:08 +0000

    Add files via upload

---
+++ 8kyu/lario.js
@@ -0,0 +1,26 @@
+// Lario  and Muigi pipe problem
+
+
+// Issue
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+// Task
+// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+
+
+function pipeFix(numbers){
+    let result = []
+    let max = Math.max(...numbers)
+    let min = Math.min(...numbers)
+    for(let i = min; i <= max; i++) {
+      result.push(i)
+    }
+    console.log(result)
+    return result
+  }
