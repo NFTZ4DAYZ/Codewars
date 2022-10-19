ecbd874ab3827a989187e30cc933b96f0b2bc665
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-10-19 01:32:17 +0000

    Add files via upload

---
+++ 7kyu/nthSmallestElement.js
@@ -0,0 +1,10 @@
+// Nth smallest element
+
+// Task
+// Given an array/list [] of integers , Find the Nth smallest element in this array of integers
+
+
+
+function nthSmallest(arr, pos){
+    return arr.sort( (a,b) => a - b)[pos - 1]
+  }
