9fe0b8d892cf1edf715994b2862621ea6db75cf1
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2022-10-21 01:33:29 +0000

    Add files via upload

---
+++ 7kyu/squaresSequence.js
@@ -0,0 +1,18 @@
+// Squares Sequence 
+
+
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+// Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+function squares(x, n) {
+    let arr= []
+    let count = x
+    for(let i = 1; i <= n; i++) {
+      arr.push(count)
+      count = count**2
+    }
+    return arr
+  }
