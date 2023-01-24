c017af19ff0b12b9977471afa7fe10f370986221
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2023-01-24 21:57:36 +0000

    Add files via upload

--- 8kyu Solidity/grassHopperSummation.sol
+++ 8kyu Solidity/grassHopperSummation.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
 
+contract Kata {
+  function summation(int n) public pure returns (int) {
+ 
+     int result = 0;
+    for(int i = 1; i <= n; i++) {
+       result += i;
+      
+    }
+     return result;
+  }
+ 
+}
+
+//Using Gauss Summation
+
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Kata {
+  function summation(int n) public pure returns (int) {
+    // your code here
+    return (n*(n + 1))/2;
+  }
+}
