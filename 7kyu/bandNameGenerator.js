11ba7cc4d8cc27426f4b854fecdc973d8fc0c00e
Dexter Matis <111626779+NFTZ4DAYZ@users.noreply.github.com>
2023-03-10 18:39:01 +0000

    Add files via upload

---
+++ 7kyu/bandNameGenerator.js
@@ -0,0 +1,2 @@
+function bandNameGenerator(str) {
+    return str[0] != str[str.length - 1] ? `The ${str[0].toUpperCase() + str.slice(1)}` : `${str[0].toUpperCase() + str.slice(1) + str.slice(1)}` }
